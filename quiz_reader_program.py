fbc8f28a7ef43b7ad407481e72c5d996f20b407f
plttoledo <toledopreciouslaine@gmail.com>
2025-05-08 03:34:47 +0800

    Added a text box for the question that cannot be edited, which is also the place where the question will be in when the file is opened.

--- quiz_reader_program.py
+++ quiz_reader_program.py
@@ -10,7 +10,7 @@ font_clr = '#542e38' # Darker Pink
 window = tk.Tk()
 
 window.geometry('600x500')
-window.title('Quiz Reader') #Title bar
+window.title('Quiz Reader') # Title bar
 
 # Aesthetics
 window.configure(bg=main_clr) # Background color
@@ -18,11 +18,19 @@ window.configure(bg=main_clr) # Background color
 header = tk.Frame(window, bg=header_clr, height=100, bd=1, relief='solid')
 header.pack(fill="x")
 
-header_label = tk.Label(header, text="Quiz Reader", bg=header_clr,
-                        fg=font_clr, font=("Trebuchet MS", 45, "bold"))
+header_label = tk.Label(header, text="Quiz Reader", bg=header_clr, fg=font_clr, font=("trebuchet MS", 45, "bold"))
 header_label.grid(padx=40, sticky="w")
 
 footer = tk.Frame(window, bg=header_clr, height=60, bd=1, relief='solid')
 footer.pack(fill="x", side='bottom')
+
+# Question Text
+question_tx = tk.Label(window, text='Question:', font=('Trebuchet MS', 25, 'bold'),
+                    fg=font_clr, bg=main_clr)
+question_tx.pack(padx=40, pady=10, anchor='w')
+
+# Question Textbox
+textbox_qstn = tk.Text(window, font=('Arial', 15), height=1, bd=1, relief='solid', state='disabled')
+textbox_qstn.pack(padx=40, anchor='w')
 
 window.mainloop()
